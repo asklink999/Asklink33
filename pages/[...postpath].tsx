2a2a3b258dc4ad0ec146829bc3d394e520c5ccd0
asklink999 <137942114+asklink999@users.noreply.github.com>
2023-07-01 20:36:15 +0530

    Update [...postpath].tsx

--- pages/[...postpath].tsx
+++ pages/[...postpath].tsx
@@ -19,7 +19,7 @@ export const getServerSideProps: GetServerSideProps = async (ctx) => {
 			redirect: {
 				permanent: false,
 				destination: `${
-					`https://askline24.blogspot.com/`
+					`https://askline24.blogspot.com/2023/06/i-like-girl-but-she-has-boyfriend-what.html`
 				}`,
 			},
 		};
